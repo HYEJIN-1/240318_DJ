f1fc99c7498081ed9d28cd953510a3ac75ea7b56
HYEJIN-1 <youthful159@naver.com>
2024-03-18 15:51:13 +0900

    Csharp

---
+++ Csharp/01/Hello00/Hello00/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hello00
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // 실행 ctrl + F5
+            // mbox적고 tab키 두번 누르면 아래 코드가 자동으로 나옴 
+            MessageBox.Show("안녕하세요");
+            MessageBox.Show("goodbye~");
+        }
+    }
+}
